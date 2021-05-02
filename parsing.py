4292437ff47687a29226bebd9f46025b75d5a7a9
Jiaory <sjz1005@gmail.com>
2021-05-01 17:44:14 -0700

    moved part3 args to read_index.py

--- parsing.py
+++ parsing.py
@@ -112,27 +112,17 @@ main_val_list = list(termInfo.values())
 doc_key_list = list(docIndexMap.keys())
 doc_val_list = list(docIndexMap.values())
 
-if len(sys.argv) != (3 or 5): 
-    raise ValueError('Please provide a query, either --doc DOCNAME or --term TERM or both.')
+def get_main_key():
+    return main_key_list
 
-if sys.argv[1] == '--doc':
-    print("Listing for document: ", sys.argv[2])
-    #FIXME: print distinct terms (optional)
-    docID = 0
-    termCount = 0
-    for docname in doc_val_list:
-        if sys.argv[2] == docname:
-            position = doc_val_list.index(docname)
-            docID = doc_key_list[position]
-    print("DOCID: ", docID)
+def get_main_val():
+    return main_val_list
 
-    for entry in map:
-        if entry[1] == docID:
-            termCount += 1 
-    print("Total terms: ", termCount)
+def get_doc_key():
+    return doc_key_list
 
-elif sys.argv[1] == '--term':
-    print("Listing for term: ", sys.argv[2])
-    #FIXME: print termID, num of docs containing term, term frequency 
+def get_doc_val():
+    return doc_val_list
 
-elif (sys.argv[1] == '--doc' and sys.argv[3] == '--term') or (sys.argv[1] == '--term' and sys.argv[3] == '--doc')
+def get_map():
+    return map
--- read_index.py
+++ read_index.py
@@ -3,4 +3,43 @@ import re
 import os
 import zipfile
 import sys
-import parsing
+from parsing import get_main_key
+from parsing import get_main_val
+from parsing import get_doc_key
+from parsing import get_doc_val
+from parsing import get_map
+
+main_key_list = get_main_key()
+main_val_list = get_main_val()
+
+doc_key_list = get_doc_key()
+doc_val_list = get_doc_val()
+
+map = get_map()
+
+if len(sys.argv) != (3 or 5): 
+    raise ValueError('Please provide a query, either --doc DOCNAME or --term TERM or both.')
+
+if sys.argv[1] == '--doc':
+    print("Listing for document: ", sys.argv[2])
+    #FIXME: print distinct terms (optional)
+    docID = 0
+    termCount = 0
+    for docname in doc_val_list:
+        if sys.argv[2] == docname:
+            position = doc_val_list.index(docname)
+            docID = doc_key_list[position]
+    print("DOCID: ", docID)
+
+    for entry in map:
+        if entry[1] == docID:
+            termCount += 1 
+    print("Total terms: ", termCount)
+
+elif sys.argv[1] == '--term':
+    print("Listing for term: ", sys.argv[2])
+    #FIXME: print termID, num of docs containing term, term frequency 
+    
+
+elif (sys.argv[1] == '--doc' and sys.argv[3] == '--term') or (sys.argv[1] == '--term' and sys.argv[3] == '--doc'):
+    print("test")
